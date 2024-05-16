3d1147c78f1cb945f4f4b2ab0188921c169661ef
AnkitBisen <bisenankit21@gmail.com>
2024-05-16 10:39:26 +0530

    Initial commit

---
+++ src/main/java/com/example/springboot/crudDemoMain/CrudDemoMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.crudDemoMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemoMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoMainApplication.class, args);
+	}
+
+}
